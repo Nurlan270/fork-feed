03f5be641b5847733f4994035659907228bf04f7
Nurlan270 <nurlanismzade@gmail.com>
2025-05-15 15:45:43 +0400

    updated

--- lang/en/flasher.php
+++ lang/en/flasher.php
@@ -2,8 +2,15 @@
 
 return [
     'auth' => [
-        'success' => 'Authentication successful',
-        'error' => 'Authentication failed. Please try again.',
+        'success' => [
+            'register' => 'Authentication successful. Please check your inbox to verify email',
+            'oauth' => 'Authentication successful'
+        ],
+        'error' => 'Authentication failed. Please try again',
+    ],
+    'email' => [
+        'success' => 'Email verified successfully',
+        'sent' => 'Email verification link sent to your email address. Please check your inbox',
     ],
     'logout' => 'Logged out',
     'invalid_credentials' => 'Invalid credentials',
