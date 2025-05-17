b2501c6bc7b92e937cfec1cbf21003e64f371b0a
Nurlan270 <nurlanismzade@gmail.com>
2025-05-17 19:19:19 +0400

    few routes for recipe, and profile route (temporary) added

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,21 @@
 
 require_once __DIR__ . '/auth.php';
 
+use App\Http\Controllers\Recipe\StoreRecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    //  Recipe
+    Route::name('recipe.')->prefix('recipes')->group(function () {
+       Route::view('create', 'recipe.create')->name('create');
+       Route::post('/', StoreRecipeController::class)->name('store');
+    });
+
+    //  Profile
+    Route::get('profile', function () {
+        $recipes = auth()->user()->recipes()->get();
+        return view('profile', compact('recipes'));
+    })->name('profile');
+});
