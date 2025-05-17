874659ee5457d57cf0866b29218d3e5b3ade3b23
Nurlan270 <nurlanismzade@gmail.com>
2025-05-17 19:17:19 +0400

    recipe creation logic added

---
+++ app/Http/Controllers/Recipe/StoreRecipeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Recipe;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Recipes\StoreRecipeRequest;
+use App\Http\Services\RecipeService;
+
+class StoreRecipeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(StoreRecipeRequest $request, RecipeService $service)
+    {
+        $request->validated();
+
+        $ingredientIds = $service->createAndGetIngredientIds($request);
+
+        $recipe = $service->createAndGetRecipe($request);
+
+        $recipe->ingredients()->sync($ingredientIds);
+
+        $service->storeImages($request, $recipe);
+
+        return redirect()->route('welcome');
+    }
+}
---
+++ app/Http/Services/RecipeService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Ingredient;
+use App\Models\Recipe;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RecipeService
+{
+    public function createAndGetIngredientIds(FormRequest $request): array
+    {
+        $ingredients = collect(json_decode($request->ingredients))
+            ->pluck('value')
+            ->filter()
+            ->unique()
+            ->values();
+
+        return $ingredients->map(function ($name) {
+            return Ingredient::firstOrCreate(['name' => $name])->id;
+        })->toArray();
+    }
+
+    public function createAndGetRecipe(FormRequest $request): Recipe
+    {
+        return $request->user()->recipes()->create([
+            'title'       => $request->title,
+            'description' => $request->description,
+        ]);
+    }
+
+    public function storeImages(FormRequest $request, Recipe $recipe): void
+    {
+        $names = collect($request->file('images'))->map(function ($image) {
+            return $image->store('/', 'recipe-images');
+        });
+
+        $recipe->images()->createMany(
+            $names->map(fn($name) => ['name' => $name])->toArray()
+        );
+    }
+}
