0f73a96aabd9c9300bae511664d7f04c60e325da
Nurlan270 <nurlanismzade@gmail.com>
2025-05-17 19:16:26 +0400

    recipe creation page added

---
+++ resources/views/recipe/create.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+
+@section('page.title', 'Create Recipe')
+
+@pushonce('styles')
+    <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.polyfills.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.css" rel="stylesheet" type="text/css"/>
+@endpushonce
+
+@section('content')
+    <main class="min-h-screen flex items-center justify-center p-4 bg-cover bg-center bg-no-repeat bg-fixed pt-24 pb-14"
+          style="background-image: url('{{ asset('media/recipe-create.jpg') }}')">
+        <div class="max-w-2xl w-full bg-white/90 rounded-lg shadow-md p-6 backdrop-blur-sm">
+            <h2 class="text-2xl font-bold text-gray-900 mb-6 text-center">
+                Create New Recipe
+            </h2>
+
+            <form method="POST" action="{{ route('recipe.store') }}" class="space-y-6" enctype="multipart/form-data">
+                @csrf
+
+                <div class="space-y-2">
+                    <label for="title" class="text-sm font-medium text-gray-700">
+                        Title
+                    </label>
+                    <input type="text" id="title" name="title" placeholder="Cheesecake"
+                           value="{{ old('title') }}"
+                           class="block w-full p-2 border rounded-md shadow-sm
+                       focus:ring-orange-500 focus:border-orange-500"
+                           required>
+                    @error('title')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="space-y-2">
+                    <label for="description" class="text-sm font-medium text-gray-700">
+                        Description
+                    </label>
+                    <x-easy-mde name="description" :options="[
+                        'minHeight' => '200px',
+                        'placeholder' => 'Cook something awesome!',
+                    ]"/>
+                    @error('description')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="space-y-2">
+                    <label class="block text-sm font-medium text-gray-700">
+                        Ingredients (After typing press "Enter")
+                    </label>
+                    <input type="text" id="ingredients" name="ingredients" class="w-full rounded-md"
+                           value="{{ old('ingredients') }}">
+                    @error('ingredients')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="space-y-2">
+                    <label class="text-sm font-medium text-gray-700">
+                        Images
+                    </label>
+                    <label
+                        class="block w-full cursor-pointer p-4 border border-dashed rounded-md text-center hover:bg-gray-50">
+                        <span id="fileLabelText" class="text-gray-600">Click to upload images (Max. 15MB)</span>
+                        <input type="file" name="images[]" id="images" class="hidden" accept="image/*" multiple>
+                    </label>
+                    @error('images')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                    @error('images.*')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <button type="submit" class="w-full flex justify-center py-2 px-4
+                    border border-transparent rounded-md shadow-sm
+                    text-sm font-medium text-white bg-orange-600
+                    hover:bg-orange-700 focus:outline-none focus:ring-2
+                    focus:ring-offset-2 focus:ring-orange-500 cursor-pointer">
+                    Create Recipe
+                </button>
+            </form>
+        </div>
+    </main>
+@endsection
+
+@pushonce('scripts')
+    <script>
+        var tagify = new Tagify(document.querySelector('#ingredients'), {
+            whitelist: []
+        });
+
+        document.querySelector('#images').addEventListener('change', function () {
+            const count = this.files.length;
+            const label = document.querySelector('#fileLabelText');
+            label.textContent = count > 0 ? `${count} image(s) selected` : 'Click to upload images (Max. 15MB)';
+        });
+    </script>
+@endpushonce
