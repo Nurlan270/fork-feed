a64e3c7777bea63c89df587593004f28256fa4bd
Nurlan270 <nurlanismzade@gmail.com>
2025-05-18 22:33:05 +0400

    name column added to users table, model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable implements CanResetPassword, MustVerifyEmail
      * @var list<string>
      */
     protected $fillable = [
+        'name',
         'username',
         'email',
         'avatar',
