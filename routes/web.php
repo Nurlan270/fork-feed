da8abc7c9a29d6fe7b12850622bba3e31e60f653
Nurlan270 <nurlanismzade@gmail.com>
2025-05-18 22:43:41 +0400

    create recipe controller added instead of just view

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 require_once __DIR__ . '/auth.php';
 
+use App\Http\Controllers\Recipe\CreateRecipeController;
 use App\Http\Controllers\Recipe\StoreRecipeController;
+use App\Models\Ingredient;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
@@ -10,13 +12,14 @@ Route::view('/', 'welcome')->name('welcome');
 Route::middleware(['auth', 'verified'])->group(function () {
     //  Recipe
     Route::name('recipe.')->prefix('recipes')->group(function () {
-       Route::view('create', 'recipe.create')->name('create');
+       Route::get('create', CreateRecipeController::class)->name('create');
        Route::post('/', StoreRecipeController::class)->name('store');
     });
 
     //  Profile
     Route::get('profile', function () {
         $recipes = auth()->user()->recipes()->get();
-        return view('profile', compact('recipes'));
+        $ingredients = Ingredient::inRandomOrder()->take(12)->get();
+        return view('profile', compact('recipes', 'ingredients'));
     })->name('profile');
 });
