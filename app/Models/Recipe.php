dbaf8657063125fe89000831fe58724075e888b6
Nurlan270 <nurlanismzade@gmail.com>
2025-05-30 17:10:26 +0400

    new relations added, removed unnecessary fillables

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\ReactionType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Recipe extends Model
@@ -15,7 +17,19 @@ class Recipe extends Model
         'user_id',
         'title',
         'description',
+        'views'
     ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'recipe_reactions')
+            ->where('type', ReactionType::LIKE);
+    }
 
     public function ingredients(): BelongsToMany
     {
