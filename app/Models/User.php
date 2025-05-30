4d6788205b3e35b43ee53e82a2d7eb5659419b6b
Nurlan270 <nurlanismzade@gmail.com>
2025-05-30 17:12:06 +0400

    new relations added

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Enums\ReactionType;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -53,6 +55,28 @@ class User extends Authenticatable implements CanResetPassword, MustVerifyEmail
     public function recipes(): HasMany
     {
         return $this->hasMany(Recipe::class)->latest();
+    }
+
+    public function reactions(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_reactions')
+            ->withPivot('type');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->reactions()->where('type', ReactionType::LIKE);
+    }
+
+    public function dislikes(): BelongsToMany
+    {
+        return $this->reactions()->where('type', ReactionType::DISLIKE);
+    }
+
+    public function bookmarks(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_bookmarks')
+            ->withTimestamps();
     }
 
     public function followers()
