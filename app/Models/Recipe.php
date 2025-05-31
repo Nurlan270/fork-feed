a567511b443de6315094ae6e670366f2d600f7e7
Nurlan270 <nurlanismzade@gmail.com>
2025-05-31 11:15:57 +0400

    sluggable column added & route key name default updated

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\ReactionType;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,14 +12,29 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Recipe extends Model
 {
     /** @use HasFactory<\Database\Factories\RecipeFactory> */
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $fillable = [
         'user_id',
         'title',
         'description',
-        'views'
+        'views',
+        'slug',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 
     public function author(): BelongsTo
     {
