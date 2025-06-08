eb1e93c4fc79b8a3728da92ccf3028a44356072d
Nurlan270 <nurlanismzade@gmail.com>
2025-06-08 12:01:24 +0400

    helper for route marking added

--- app/helpers.php
+++ app/helpers.php
@@ -6,12 +6,21 @@ use Illuminate\Support\Str;
 if (!function_exists('getUsernameSlug')) {
     function getUsernameSlug(string $username, bool $checkForExistence = false): string
     {
-        $username = Str::of($username)->trim()->lower()->replaceMatches('/\s+/', '_');
+        $username = Str::of($username)->trim()->lower()->slug('_');
 
         if ($checkForExistence && User::where('username', $username)->exists()) {
             $username .= Str::random(2);
         }
 
         return $username;
+    }
+}
+
+if (!function_exists('markRoute')) {
+    function markRoute(string $routeName, string $classes)
+    {
+        if (Route::is($routeName)) {
+            return $classes;
+        }
     }
 }
