59f53abafa1ca287399a82a3c1d252700507d009
Nurlan270 <nurlanismzade@gmail.com>
2025-06-09 21:09:46 +0400

    new relations added, made sluggable, and searchable

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Scout\Searchable;
 
 class Recipe extends Model
 {
     /** @use HasFactory<\Database\Factories\RecipeFactory> */
-    use HasFactory, Sluggable;
+    use HasFactory, Sluggable, Searchable;
 
     protected $fillable = [
         'user_id',
@@ -26,8 +27,8 @@ class Recipe extends Model
     {
         return [
             'slug' => [
-                'source' => 'title'
-            ]
+                'source' => 'title',
+            ],
         ];
     }
 
@@ -36,9 +37,22 @@ class Recipe extends Model
         return 'slug';
     }
 
+    public function toSearchableArray(): array
+    {
+        return array_merge($this->toArray(), [
+            'id'         => (string)$this->id,
+            'created_at' => $this->created_at->timestamp,
+        ]);
+    }
+
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function reactions()
+    {
+        return $this->hasMany(RecipeReaction::class);
     }
 
     public function likes(): BelongsToMany
@@ -52,8 +66,18 @@ class Recipe extends Model
         return $this->belongsToMany(Ingredient::class);
     }
 
+    public function limitedIngredients()
+    {
+        return $this->belongsToMany(Ingredient::class)->limit(8);
+    }
+
     public function images()
     {
         return $this->hasMany(RecipeImage::class);
+    }
+
+    public function firstImage()
+    {
+        return $this->hasOne(RecipeImage::class)->orderBy('id');
     }
 }
