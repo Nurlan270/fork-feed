370e6805d331089381f9a7c7d485689f4f086dc2
Nurlan270 <nurlanismzade@gmail.com>
2025-06-09 21:11:45 +0400

    new route added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/recipe.php';
 require_once __DIR__ . '/user.php';
 
 Route::view('/', 'welcome')->name('welcome');
+
+Route::view('explore', 'explore')->name('explore');
