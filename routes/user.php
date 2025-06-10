0a59eb4d22b3e8c747c109ca07b4448919019177
Nurlan270 <nurlanismzade@gmail.com>
2025-06-10 21:00:51 +0400

    some controllers replaced with livewire components

--- routes/user.php
+++ routes/user.php
@@ -1,21 +1,19 @@
 <?php
 
-use App\Http\Controllers\User\{
-    UserBookmarksController,
-    UserFollowController,
-    UserProfileController
-};
+use App\Livewire\User\Bookmarks;
+use App\Livewire\User\Profile;
+use App\Http\Controllers\User\UserFollowController;
 
-Route::get('profile', UserProfileController::class)
+Route::get('profile', Profile::class)
     ->middleware('auth')
     ->name('user.profile');
 
 Route::name('user.')->prefix('@{user:username}')->group(function () {
-    Route::get('/', UserProfileController::class)->name('tag-profile');
+    Route::get('/', Profile::class)->name('tag-profile');
     Route::get('following', [UserFollowController::class, 'following'])->name('following');
     Route::get('followers', [UserFollowController::class, 'followers'])->name('followers');
 });
 
-Route::get('bookmarks', UserBookmarksController::class)
+Route::get('bookmarks', Bookmarks::class)
     ->middleware(['auth', 'verified'])
     ->name('user.bookmarks');
