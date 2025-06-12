b51b4b65b829d6864340d3679a7dc2a3d5812319
Nurlan270 <nurlanismzade@gmail.com>
2025-06-12 18:04:44 +0400

    updated

--- app/Livewire/Explore.php
+++ app/Livewire/Explore.php
@@ -6,8 +6,6 @@ use App\Enums\ReactionType;
 use App\Models\Recipe;
 use Illuminate\Contracts\View\View;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Storage;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -27,21 +25,6 @@ class Explore extends Component
         return view('livewire.explore', [
             'recipes' => $this->recipes(),
         ]);
-    }
-
-    public function deleteRecipe(string $id): void
-    {
-        $recipe = Recipe::findOrFail($id);
-
-        Gate::authorize('delete', $recipe);
-
-        $imagePaths = $recipe->images()->chunkMap(fn($image) => $image->relativePath())->toArray();
-
-        Storage::delete($imagePaths);
-
-        $recipe->delete();
-
-        notyf()->success(__('flasher.recipe.deleted'));
     }
 
     public function updated($property): void
