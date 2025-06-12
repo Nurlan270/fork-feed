196e6fd4104171bea8a2cbc5bec630fe409b9a18
Nurlan270 <nurlanismzade@gmail.com>
2025-06-12 18:03:44 +0400

    updated

---
+++ app/Livewire/ShowBookmarks.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class ShowBookmarks extends Component
+{
+    use WithPagination, WithoutUrlPagination;
+
+    #[On('bookmark-removed')]
+    public function render()
+    {
+        $bookmarks = auth()->user()->bookmarks()
+            ->with('firstImage')
+            ->latest()
+            ->paginate(2);
+
+        if ($bookmarks->isEmpty() && $bookmarks->currentPage() > 1) {
+            $this->previousPage();
+        }
+
+        return view('livewire.show-bookmarks', compact('bookmarks'));
+    }
+}
