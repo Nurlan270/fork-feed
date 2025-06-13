1da89551db80e653b9b89d59dba72ef857ae9c92
Nurlan270 <nurlanismzade@gmail.com>
2025-06-13 22:13:34 +0400

    styles updated to support scroll effect

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 right-0 z-50 transition-transform duration-300 ease-in-out" id="navbar">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="{{ route('welcome') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset('media/logo.png') }}" class="h-8" alt="ForkFeed Logo"/>
