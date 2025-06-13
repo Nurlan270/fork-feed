a6da08847dd0ba96e63b5ef36ac2cf3742a795f6
Nurlan270 <nurlanismzade@gmail.com>
2025-06-13 13:03:14 +0400

    user recipes livewire component added

---
+++ app/Livewire/ShowUserRecipes.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Ingredient;
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowUserRecipes extends Component
+{
+    use WithPagination;
+
+    public ?User $user;
+
+    public function mount(): void
+    {
+        $this->user ??= auth()->user();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.show-user-recipes', [
+            'recipes'     => $this->recipes(),
+            'ingredients' => $this->ingredients(),
+        ]);
+    }
+
+    protected function recipes(): LengthAwarePaginator
+    {
+        return Recipe::with(['firstImage', 'limitedIngredients'])
+            ->where('user_id', $this->user->id)
+            ->latest()
+            ->paginate(25);
+    }
+
+    protected function ingredients(): Collection
+    {
+        return Ingredient::withCount([
+            'recipes' => function ($query) {
+                $query->where('user_id', $this->user->id);
+            }])
+            ->whereHas('recipes', function ($query) {
+                $query->where('user_id', $this->user->id);
+            })
+            ->orderByDesc('recipes_count')
+            ->limit(20)
+            ->get();
+    }
+}
