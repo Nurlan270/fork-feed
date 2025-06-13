23a00db757a618cde550c9511a76e6818d725289
Nurlan270 <nurlanismzade@gmail.com>
2025-06-13 13:05:25 +0400

    unnecessary route & namespaces removed

--- routes/recipe.php
+++ routes/recipe.php
@@ -1,19 +1,19 @@
 <?php
 
 use App\Http\Middleware\IncrementViewsNumber;
-use App\Http\Controllers\Recipe\{CreateRecipeController,
-    DeleteRecipeController,
+use App\Http\Controllers\Recipe\{
+    CreateRecipeController,
     EditRecipeController,
     ShowRecipeController,
     StoreRecipeController,
-    UpdateRecipeController};
+    UpdateRecipeController
+};
 
 Route::name('recipe.')->prefix('recipes')->middleware(['auth', 'verified'])->group(function () {
     Route::get('create', CreateRecipeController::class)->name('create');
     Route::post('/', StoreRecipeController::class)->name('store');
     Route::get('{recipe}/edit', EditRecipeController::class)->name('edit');
     Route::patch('{recipe}', UpdateRecipeController::class)->name('update');
-    Route::delete('{recipe}', DeleteRecipeController::class)->name('delete');
 });
 
 Route::get('recipes/{recipe}', ShowRecipeController::class)
