fba3539ad9228ede135da1d1191a8e7ee288fe5d
Nurlan270 <nurlanismzade@gmail.com>
2025-06-13 17:51:14 +0400

    new livewire component added

---
+++ app/Livewire/ShowRecipesWithIngredient.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Enums\ReactionType;
+use App\Models\Ingredient;
+use App\Models\Recipe;
+use Illuminate\Contracts\View\View;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Attributes\Url;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowRecipesWithIngredient extends Component
+{
+    use WithPagination;
+
+    public Ingredient $ingredient;
+
+    #[Url(as: 'q')]
+    public string $search = '';
+
+    #[Url]
+    public string $sort = '';
+
+    public function render(): View
+    {
+        return view('livewire.show-recipes-with-ingredient', [
+            'recipes' => $this->recipes(),
+        ]);
+    }
+
+    public function updated($property): void
+    {
+        if (in_array($property, ['search', 'sort'])) {
+            $this->resetPage();
+        }
+    }
+
+    protected function recipes(): LengthAwarePaginator
+    {
+        $query = Recipe::with(['firstImage', 'author', 'limitedIngredients'])
+            ->withCount([
+                'reactions as likes_count'    => fn($q) => $q->where('type', ReactionType::LIKE),
+                'reactions as dislikes_count' => fn($q) => $q->where('type', ReactionType::DISLIKE),
+            ])
+            ->whereHas('ingredients', function ($q) {
+                $q->where('slug', $this->ingredient->slug);
+            })
+            ->orderByRaw($this->getSortColumn());
+
+        if ($this->search) {
+            $ids = Recipe::search($this->search)->get()->pluck('id');
+
+            $query->whereIn('id', $ids);
+        }
+
+        return $query->paginate(50);
+    }
+
+    protected function getSortColumn(): string
+    {
+        return match ($this->sort) {
+            'popular' => '(likes_count - dislikes_count) + recipes.views DESC',
+            'liked'   => 'likes_count DESC',
+            'newest'  => 'recipes.created_at DESC',
+            'oldest'  => 'recipes.created_at ASC',
+            default   => 'recipes.views DESC',
+        };
+    }
+}
