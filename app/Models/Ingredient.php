19baf52fcd7e03b194ee067b8a4ea5852daf726e
Nurlan270 <nurlanismzade@gmail.com>
2025-06-13 17:38:38 +0400

    made sluggable & mutator added

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,16 +11,33 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Ingredient extends Model
 {
     /** @use HasFactory<\Database\Factories\IngredientFactory> */
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     public $timestamps = false;
 
     protected $fillable = [
-        'name'
+        'name',
+        'slug',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ],
+        ];
+    }
 
     public function recipes(): BelongsToMany
     {
         return $this->belongsToMany(Recipe::class);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::set(
+            fn($value) => ucfirst(($value),
+        ));
     }
 }
