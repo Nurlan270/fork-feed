30ff6dd8acda128984e088bb537719104c3f5c8f
Nurlan270 <nurlanismzade@gmail.com>
2025-06-16 22:59:39 +0400

    middleware aliases for localization middlewares added & default Guest redirect URL changed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter;
+use Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes;
+use Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-//        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'localize'             => LaravelLocalizationRoutes::class,
+            'localizationRedirect' => LaravelLocalizationRedirectFilter::class,
+            'localeCookieRedirect' => LocaleCookieRedirect::class,
+        ]);
+
+        $middleware->redirectGuestsTo('auth/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
