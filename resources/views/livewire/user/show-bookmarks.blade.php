5de4497ad4bdfc0bf3e29d15f2a405a236163ae6
Nurlan270 <nurlanismzade@gmail.com>
2025-06-21 21:39:06 +0400

    unnecessary text removed

--- resources/views/livewire/user/show-bookmarks.blade.php
+++ resources/views/livewire/user/show-bookmarks.blade.php
@@ -13,15 +13,6 @@
     @else
         <div class="text-center mt-16">
             <p class="text-gray-600 text-base mb-2">{{ __('bookmarks.empty') }}</p>
-            <p class="text-gray-600 text-base flex justify-center items-center gap-1">
-                {{ __('bookmarks.hint') }}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3.5"
-                     stroke="currentColor" class="size-3.5 mx-1 text-primary-600">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"/>
-                </svg>
-                {{ __('bookmarks.button_hint') }}
-            </p>
         </div>
     @endif
 </div>
