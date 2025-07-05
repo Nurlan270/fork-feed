a1c6e93d2612cc10b84db0ac2c85b8b25d014c5e
Nurlan270 <nurlanismzade@gmail.com>
2025-07-05 15:46:49 +0400

    ingredients page's link added

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -179,13 +179,23 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul class="flex flex-col gap-y-2 font-medium p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                 <li>
-                    <a href="{{ getLocalizedURL('explore') }}"
+                    <a href="{{ getLocalizedURL('recipes') }}"
                        class="block py-2 px-3 text-gray-900 rounded-sm
                               md:p-0 md:bg-transparent md:hover:bg-transparent
                               hover:bg-gray-100
                               hover:text-primary-700
                               {{ markRoute('explore', 'text-white bg-primary-700 md:text-primary-700') }}">
-                        {{ __('navbar.explore') }}
+                        {{ __('navbar.recipes') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ getLocalizedURL('ingredients') }}"
+                       class="block py-2 px-3 text-gray-900 rounded-sm
+                              md:p-0 md:bg-transparent md:hover:bg-transparent
+                              hover:bg-gray-100
+                              hover:text-primary-700
+                              {{ markRoute('explore', 'text-white bg-primary-700 md:text-primary-700') }}">
+                        {{ __('navbar.ingredients') }}
                     </a>
                 </li>
                 <li>
